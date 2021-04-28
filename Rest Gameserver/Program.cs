d53084d7107fd24c1b0b8b05da94c829ded39113
Casper Scherp <C_scherp@hotmail.se>
2021-04-28 10:09:04 +0200

    Adds cs.proj and Folders

---
+++ Rest Gameserver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gp20_2021_0426_rest_gameserver_CasperScherp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
